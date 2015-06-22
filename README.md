f1028d5c0c777f95f9f18038611ccd1dd869c900
noamokman <noamokman@gmail.com>
2015-06-22 22:39:49 +0300

    added jscs, initial work on jshint and testing

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # nachos-home
 Get the path to the nachos home directory
+
+<table>
+  <thead>
+    <tr>
+      <th>Linux</th>
+      <th>OS X</th>
+      <th>Windows</th>
+      <th>Coverage</th>
+      <th>Dependencies</th>
+      <th>Dev-Dependencies</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td colspan="2" align="center">
+        <a href="https://travis-ci.org/nachos/nachos-home"><img src="https://travis-ci.org/nachos/nachos-home.svg"></a>
+      </td>
+      <td align="center">
+        <a href="https://ci.appveyor.com/project/nachos/nachos-home"><img src="https://ci.appveyor.com/api/projects/status/null?svg=true"></a>
+      </td>
+      <td align="center">
+        <a href="https://coveralls.io/r/nachos/nachos-home"><img src="https://coveralls.io/repos/nachos/nachos-home/badge.svg" alt="Coverage Status"></a>
+      </td>
+      <td align="center">
+        <a href="https://david-dm.org/nachos/nachos-home"><img src="https://david-dm.org/nachos/nachos-home.svg"></a>
+      </td>
+      <td align="center">
+        <a href="https://david-dm.org/nachos/nachos-home#info=devDependencies"><img src="https://david-dm.org/nachos/nachos-home/dev-status.svg"/></a>
+      </td>
+    </tr>
+  </tbody>
+</table>
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,97 @@
+/** Regular npm dependendencies */
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var lazypipe = require('lazypipe');
+var runSequence = require('run-sequence');
+var open = require('open');
+
+/** Gulp dependencies */
+var gutil = require('gulp-util');
+var bump = require('gulp-bump');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+/** Reusable functions */
+
+function jshintPipe(jshintrc) {
+  return lazypipe()
+    .pipe(jshint, jshintrc)
+    .pipe(jshint.reporter, stylish)
+    .pipe(jshint.reporter, 'fail')();
+}
+/** Gulp tasks */
 
 gulp.task('default', ['test']);
 
-gulp.task('test', ['jshint']);
+gulp.task('test', function () {
+  runSequence(
+    'jshint:all',
+    'mocha',
+    function () {
+      process.exit(0);
+    });
+});
+
+gulp.task('jshint:all', ['jshint', 'jshint:test', 'jshint:gulpfile']);
 
 gulp.task('jshint', function () {
-  gulp.src('./lib/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter(stylish))
-    .pipe(jshint.reporter('fail'));
+  return gulp.src(['lib/**/*.js'])
+    .pipe(jshintPipe('.jshintrc'));
+});
+
+gulp.task('jshint:test', function () {
+  return gulp.src(['test/**/*.js'])
+    .pipe(jshintPipe('.spec.jshintrc'));
+});
+
+gulp.task('jshint:gulpfile', function () {
+  return gulp.src('gulpfile.js')
+    .pipe(jshintPipe('.jshintrc'));
+});
+
+gulp.task('jscs:all', ['jscs', 'jscs:test', 'jscs:gulpfile']);
+
+gulp.task('jscs', function () {
+  return gulp.src('lib/**/*.js')
+    .pipe(jscs());
+});
+
+gulp.task('jscs:test', function () {
+  return gulp.src('./test/**/*.js')
+    .pipe(jscs());
+});
+
+gulp.task('jscs:gulpfile', function () {
+  return gulp.src('./gulpfile.js')
+    .pipe(jscs());
+});
+
+gulp.task('mocha', ['env:test', 'env:local'], function (cb) {
+  gulp.src('test/**/*.spec.js', {read: false})
+    .pipe(mocha({reporter: 'spec'}))
+    .on('error', function (err) {
+      gutil.log(err.toString());
+    })
+    .once('end', function () {
+      cb();
+    });
+});
+
+gulp.task('bump', function () {
+  gulp.src(['./package.json'])
+    .pipe(bump({type: 'patch'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:minor', function () {
+  gulp.src(['./package.json'])
+    .pipe(bump({type: 'minor'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:major', function () {
+  gulp.src(['./package.json'])
+    .pipe(bump({type: 'major'}))
+    .pipe(gulp.dest('./'));
 });
