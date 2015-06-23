4facbc48d2d6c9373966faaa1fb4e261309adc94
noamokman <noamokman@gmail.com>
2015-06-23 22:56:30 +0300

    added coveralls

--- gulp/config.js
+++ gulp/config.js
@@ -4,7 +4,8 @@ module.exports = {
   paths: {
     lib: './lib/**/*.js',
     test: './test/**/*.spec.js',
-    gulp: ['./gulpfile.js', './gulp/**/*.js']
+    gulp: ['./gulpfile.js', './gulp/**/*.js'],
+    coverage: 'coverage/**/lcov.info'
   },
   manifests: ['./package.json']
 };
--- gulp/tasks/bump.js
+++ gulp/tasks/bump.js
@@ -5,19 +5,19 @@ var config = require('../config');
 
 module.exports = function (gulp) {
   gulp.task('bump', function () {
-    gulp.src(config.manifests)
+    return gulp.src(config.manifests)
       .pipe(bump({type: 'patch'}))
       .pipe(gulp.dest('./'));
   });
 
   gulp.task('bump:minor', function () {
-    gulp.src(config.manifests)
+    return gulp.src(config.manifests)
       .pipe(bump({type: 'minor'}))
       .pipe(gulp.dest('./'));
   });
 
   gulp.task('bump:major', function () {
-    gulp.src(config.manifests)
+    return gulp.src(config.manifests)
       .pipe(bump({type: 'major'}))
       .pipe(gulp.dest('./'));
   });
